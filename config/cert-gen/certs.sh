ae292e21a59cb6c4cfe55cb56a6b2dc9a4053f20
yuudj <ricardo.brea@unahur.edu.ar>
2023-06-11 19:55:00 -0300

    Servicio Generacion de certificados compose: nuevo volumen compose:  script-gen se corre una imagen que genera certificados certs.sh: genera lso certificados necesairos en el volumen certs

---
+++ config/cert-gen/certs.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# GENERA CERTIFICADO SELF-SIGNED PARA iot.localdev.me
+if [ ! -f /certificates/iot.localdev.me.key ]
+then
+  cd /certificates
+
+  openssl req -x509 -newkey rsa:2048  -days 3650 -nodes \
+    -keyout iot.localdev.me.key -out iot.localdev.me.crt -subj "/CN=iot.localdev.me" \
+    -addext "subjectAltName=DNS:iot.localdev.me"
+
+  echo "iot.localdev.me.key Certificates generated"
+else
+  echo "iot.localdev.me.key Certificates already generated"
+fi
+
+
+# GENERA CERTIFICADO SELF-SIGNED PARA influxdb.localdev.me
+if [ ! -f /certificates/influxdb.localdev.me.key ]
+then
+  cd /certificates
+
+  openssl req -x509 -newkey rsa:2048  -days 3650 -nodes \
+    -keyout influxdb.localdev.me.key -out influxdb.localdev.me.crt -subj "/CN=influxdb.localdev.me" \
+    -addext "subjectAltName=DNS:influxdb.localdev.me"
+
+  echo "influxdb.localdev.me.key Certificates generated"
+else
+  echo "influxdb.localdev.me.key Certificates already generated"
+fi
